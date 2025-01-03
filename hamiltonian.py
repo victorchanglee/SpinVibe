6a0d70eff81325d7fca217303c666cca1e91ff13
victorchanglee <victorchanglee@hotmail.com>
2025-01-03 17:15:29 -0600

    update

--- hamiltonian.py
+++ hamiltonian.py
@@ -6,20 +6,15 @@ def first_finite_diff(f, x, delta):
 
     Parameters:
         f (function): Function to differentiate.
-        x (array): Coordinates at which to evaluate the derivative.
+        x (float or array): Point(s) at which to evaluate the first derivative.
         delta (float): Small step size for finite difference.
 
     Returns:
         array: First derivative of f at x.
     """
-    derivatives = np.zeros_like(x)
-    for i in range(len(x)):
-        x_forward = x.copy()
-        x_backward = x.copy()
-        x_forward[i] += delta
-        x_backward[i] -= delta
-        derivatives[i] = (f(x_forward) - f(x_backward)) / (2 * delta)
-    return derivatives
+    derivative_first = (f(x + delta) - f(x - delta)) / (delta ** 2)
+
+    return derivative_first
 
 def second_finite_diff(f,x,delta):
     """
@@ -33,9 +28,9 @@ def second_finite_diff(f,x,delta):
     Returns:
         float or array: Second derivative of f at x.
     """
-    finite_second = (f(x + delta) - 2 * f(x) + f(x - delta)) / (delta ** 2)
+    derivative_second = (f(x + delta) - 2 * f(x) + f(x - delta)) / (delta ** 2)
 
-    return finite_second
+    return derivative_second
 
 
 
